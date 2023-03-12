13dd83f83f7c05ec319fc852fda620658523e7ce
Luis Rosales <lurosales@doitt.nyc.gov>
2023-03-11 22:37:13 -0500

    *Part 1 - Calculate Length, Sum and Average

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -17,17 +17,20 @@ loan_costs = [500, 600, 200, 1000, 450]
 # How many loans are in the list?
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
-# YOUR CODE HERE!
+number_of_loans = len(loan_costs)
+print("The number of loans is {number_of_loans}")
 
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
-# YOUR CODE HERE!
+sum_of_loans = sum(loan_costs)
+print("The sum of loans is {sum_of_loans}")
 
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
-# YOUR CODE HERE!
+avg_loan = sum_of_loans/number_of_loans
+print("The average loan is {avg_loan}")
 
 """Part 2: Analyze Loan Data.
 
